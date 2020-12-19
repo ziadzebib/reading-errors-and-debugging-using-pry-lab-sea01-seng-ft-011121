4fd18f27107366947fa8a948eee61794cb700ae2
Ziad Zebib <zebibziad@gmail.com>
2020-12-19 03:37:00 +0000

    Done.

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,4 +1,4 @@
-# don't forget to add: require 'pry'
+
 
 def get_user_input
   gets.chomp
@@ -11,11 +11,11 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
-  end
+  if num == 1
+    "YUM YUM MUNCH MUNCH MUNCH"
+  elsif num == 2
+    "HAM HAM HAM IN MY TUMMY"
+  end   
 end
 
 def runner
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,9 @@
 # don't forget to add: require 'pry'
 
-def snake_it_up(string)
+def snake_it_up(string = suprise)
   if string[0] == "s"
-    10 * "s" + string
+    "ssssssssss" + string
   else
     string
-  end
+  end  
 end
