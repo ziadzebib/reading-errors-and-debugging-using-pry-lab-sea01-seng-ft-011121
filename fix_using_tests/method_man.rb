c71d0b3cea8755d72ecd6ad9f9b8961fee4722ba
Ziad Zebib <zebibziad@gmail.com>
2020-12-19 02:37:25 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,10 +1,9 @@
 # don't forget to add: require 'pry'
 
-def start_game(player1, player2)
+def start_game(player1 = Rocky, player2 = Bullwinkle)
   "Hello #{player1} & #{player2}"
 end
 
-def play_game(player2)
+def play_game(player1 = Jay-Z, player2)
   "#{player1} is better than #{player2}"
 end
-puts = "#{player1} is better than #{player2}"
