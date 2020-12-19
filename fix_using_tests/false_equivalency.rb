14d5594897fb2f6a671fc98729033aa311007fd0
Ziad Zebib <zebibziad@gmail.com>
2020-12-19 03:23:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,9 +12,9 @@ end
 
 def selection(num)
   if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
+    puts = "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+    puts = "HAM HAM HAM IN MY TUMMY"
   end   
 end
 
