8d9a8910505364bfb26fa4eda8ca5ac822a9fa68
Ziad Zebib <zebibziad@gmail.com>
2020-12-19 02:43:29 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
 
-def snake_it_up(string)
+def snake_it_up(string = suprise)
   if string[0] == "s"
-    10 * "s.to_1" + string
+    10 * "s" + string
   else
     string
   end  
